6ba3506ff796c99ef49845197534e0945977314d
Mertan Rasim <mertan.rasim@gmail.com>
2024-05-22 12:26:24 +0200

    Added testing

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://api.respotify.com',
+  baseURL: import.meta.env.BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
